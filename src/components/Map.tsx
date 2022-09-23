eeb82898f728c1605e3c85b1c55d845795a733c0
hundehausen <papier@grischadaum.de>
2022-09-23 12:32:20 +0200

    remove debugger

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -4,7 +4,6 @@ import type { Node } from '@prisma/client';
 import { Box, Heading } from '@chakra-ui/react';
 
 const Map = ({ nodes }: { nodes: Node[] }) => {
-  debugger;
   return (
     <>
       <Box p={8}>
